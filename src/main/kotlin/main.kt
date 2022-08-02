1892aabcdd5726c7eb4c4e9943fd7c778490ef98
nohreit <hierno.mah@gmail.com>
2022-08-02 15:48:39 -0400

    progressed on leetcode explorer tableau 101. Stoppet at the first challenge of the conclusion chapter. Will continue next time (probably the next day)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -76,7 +76,7 @@ fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
     println("nums1: ${nums1.toList()} | nums2: ${nums2.toList()}")
 }
 
-fun removeElement(nums: IntArray, `val`: Int): Int {
+fun removeElementFirst(nums: IntArray, `val`: Int): Int {
     var k = 0
     for (i in nums.indices) {
         if (nums[i] != `val`) {
@@ -293,18 +293,186 @@ first apply the idea discussed using an additional array and the in-place soluti
 Hide Hint #2
 A two-pointer approach could be helpful here. The idea would be to have one pointer for iterating the array and another
 pointer that just works on the non-zero elements of the array.
+ */
+
+/*
+    fun moveZeroes(nums: IntArray): Unit {
+        var i = 0
+
+        for (j in (0..nums.lastIndex)) {
+            if (nums[j] != 0) {
+                nums[i] = nums[j]
+
+                if (i != j) {
+                    nums[j] = 0
+                }
+                i++
+            }
+        }
+    }
 
  */
 
 fun moveZeroes(nums: IntArray) {
-//    if (nums.isEmpty()) return
-//    if (nums.size == 1) print(nums[0])
-//    var a = 0
-//    for(i in nums.indices){
-//        if(nums[i] == 0){
-//            a = i
-//        }else
-//    }
+    if (nums.isEmpty() || nums.size == 1) return
+
+    var j = 0
+
+    for (i in nums.indices) {
+        if (nums[i] != 0 && nums[j] == 0) {
+            nums[j] = nums[i].also { nums[i] = nums[j] }
+            j++
+        } else if (nums[i] == 0 && nums[j] != 0) j = i
+    }
+}
+
+/***
+ * Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+Return any array that satisfies this condition.
+
+
+
+Example 1:
+
+Input: nums = [3,1,2,4]
+Output: [2,4,3,1]
+Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
+
+
+Constraints:
+
+1 <= nums.length <= 5000
+0 <= nums[i] <= 5000
+ */
+
+/*
+fun sortArrayByParity(nums: IntArray): IntArray {
+    var left = 0
+    var right = nums.size - 1
+    while (left < right) {
+        val isLeftOdd = nums[left] % 2 == 1
+        val isRightEven = nums[right] % 2 == 0
+        if (isLeftOdd && isRightEven) {
+            val temp = nums[left]
+            nums[left] = nums[right]
+            nums[right] = temp
+            left++
+            right--
+        }
+
+        if (!isLeftOdd) {
+            left++
+        }
+        if (!isRightEven) {
+            right--
+        }
+    }
+
+    return nums
+}
+
+*/
+
+fun sortArrayByParity(nums: IntArray): IntArray {
+    if (nums.isEmpty() || nums.size == 1) return nums
+
+    var j = 0
+
+    for (i in nums.indices) {
+        val isEven: Boolean = (nums[i] % 2 == 0) // true
+        val isOdd: Boolean = (nums[j] % 2 != 0) // true
+        if (isEven && isOdd) {
+            nums[j] = nums[i].also { nums[i] = nums[j] }
+            j++
+        } else if (!isEven && !isOdd) j = i
+    }
+    return nums
+}
+
+/***
+ * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int val = ...; // Value to remove
+int[] expectedNums = [...]; // The expected answer with correct length.
+// It is sorted with no values equaling val.
+
+int k = removeElement(nums, val); // Calls your implementation
+
+assert k == expectedNums.length;
+sort(nums, 0, k); // Sort the first k elements of nums
+for (int i = 0; i < actualLength; i++) {
+assert nums[i] == expectedNums[i];
+}
+
+If all assertions pass, then your solution will be accepted.
+
+
+
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Example 2:
+
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+Note that the five elements can be returned in any order.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+
+Constraints:
+
+0 <= nums.length <= 100
+0 <= nums[i] <= 50
+0 <= val <= 100
+
+Show Hint #1
+The problem statement clearly asks us to modify the array in-place and it also says that the element beyond the new length of the array can be anything. Given an element, we need to remove all the occurrences of it from the array. We don't technically need to remove that element per-say, right?
+Show Hint #2
+We can move all the occurrences of this element to the end of the array. Use two pointers!
+Show Hint #3
+Yet another direction of thought is to consider the elements to be removed as non-existent. In a single pass, if we keep copying the visible elements in-place, that should also solve this problem for us.
+
+ */
+/*
+
+
+ */
+fun removeElement(nums: IntArray, `val`: Int): Int {
+    if (nums.isEmpty()) return 0
+    if (nums.size == 1) return 1
+
+    var k = 0
+    for (i in nums.indices) {
+        if (nums[i] != `val`) {
+            nums[k] = nums[i]
+            k++
+        }
+    }
+    return k
 }
 
 fun main(args: Array<String>) {
@@ -320,6 +488,12 @@ fun main(args: Array<String>) {
     val nums9 = intArrayOf(17, 18, 5, 4, 6, 1); // Input array false
     val nums10 = intArrayOf(1, 1, 2)
     val nums11 = intArrayOf(0, 0, 1, 1, 1, 2, 2, 3, 3, 4)
+    val nums12 = intArrayOf(0, 1, 0, 3, 12)
+    val nums13 = intArrayOf(0)
+    val nums14 = intArrayOf(1, 2, 0, 9, 0, 0, 0, 13)
+    val nums15 = intArrayOf(1, 4, 9, 0, 13, 0, 0, 0, 15, 0, 19)
+    val nums16 = intArrayOf(3, 1, 2, 4)
+    val nums17 = intArrayOf(1, 4, 9, 11, 13, 5, 8, 6, 15, 3, 19)
 
     val target = 1; // Value to remove
     val expectedNums = intArrayOf(); // The expected answer with correct length.
@@ -338,7 +512,6 @@ fun main(args: Array<String>) {
         }
 
      */
-    println("test")
 
 
 }
